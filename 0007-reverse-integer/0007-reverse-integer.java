3a232f92d173a698da2fdc4965843f06b224407b
Sangaami <msangaami@gmail.com>
2025-04-06 12:58:26 +0530

    Time: 1 ms (83.54%), Space: 41.2 MB (33.69%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.java
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -1,19 +1,17 @@
 class Solution {
     public int reverse(int x) {
-        long result = 0; // Using long to handle potential overflow
+        long result = 0;
         
         while (x != 0) {
-            int digit = x % 10; // Extract the last digit
-            x /= 10; // Remove the last digit
-            
-            result = result * 10 + digit; // Form the reversed number
+            int digit = x % 10;
+            x /= 10;
+            result = result * 10 + digit; 
         }
         
-        // Check if result is within the 32-bit integer range
         if (result > Integer.MAX_VALUE || result < Integer.MIN_VALUE) {
             return 0;
         }
         
-        return (int) result; // Cast to int and return the result
+        return (int) result; 
     }
 }
