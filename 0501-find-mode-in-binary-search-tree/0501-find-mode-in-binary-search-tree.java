71abbf873cb8b3131800804b1615b5e66c50acb6
Sangaami <msangaami@gmail.com>
2025-07-28 21:02:39 +0530

    Time: 0 ms (100%), Space: 45.2 MB (61.95%) - LeetHub

--- 0501-find-mode-in-binary-search-tree/0501-find-mode-in-binary-search-tree.java
+++ 0501-find-mode-in-binary-search-tree/0501-find-mode-in-binary-search-tree.java
@@ -1,27 +1,35 @@
-import java.util.*;
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 
 class Solution {
     private Integer prev = null;
     private int count = 1;
     private int max = 0;
     private List<Integer> modes = new ArrayList<>();
-
     public int[] findMode(TreeNode root) {
         inorder(root);
-        // Convert list to array
         int[] res = new int[modes.size()];
         for (int i = 0; i < modes.size(); i++) {
             res[i] = modes.get(i);
         }
         return res;
     }
-
     private void inorder(TreeNode root) {
         if (root == null) return;
-
         inorder(root.left);
-
-        // Process current node
         if (prev != null) {
             if (root.val == prev) {
                 count++;
