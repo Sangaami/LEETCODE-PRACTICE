c07e0ecf8f86a2ff093d9b9f4bc0ddbd66f75def
Sangaami <msangaami@gmail.com>
2025-08-07 16:01:31 +0530

    Time: 50 ms (51.82%), Space: 42.2 MB (97.29%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.java
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.java
@@ -1,26 +1,25 @@
 class Solution {
     public int longestPalindromeSubseq(String s) {
-        int[][] memo = new int[s.length()][s.length()];
-        return helper(s, 0, s.length() - 1, memo);
+        String reversed = new StringBuilder(s).reverse().toString();
+        return lcs(s, reversed);
     }
 
-    private int helper(String s, int i, int j, int[][] memo) {
-        // Base case: invalid range
-        if (i > j) return 0;
-        // Base case: single character
-        if (i == j) return 1;
+    private int lcs(String a, String b) {
+        int m = a.length(), n = b.length();
+        int[] prev = new int[n + 1], curr = new int[n + 1];
 
-        // If already computed, return the cached result
-        if (memo[i][j] != 0) return memo[i][j];
-
-        // If matching characters
-        if (s.charAt(i) == s.charAt(j)) {
-            memo[i][j] = 2 + helper(s, i + 1, j - 1, memo);
-        } else {
-            // If not matching, try both directions
-            memo[i][j] = Math.max(helper(s, i + 1, j, memo), helper(s, i, j - 1, memo));
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (a.charAt(i - 1) == b.charAt(j - 1))
+                    curr[j] = 1 + prev[j - 1];
+                else
+                    curr[j] = Math.max(prev[j], curr[j - 1]);
+            }
+            int[] temp = prev;
+            prev = curr;
+            curr = temp;
         }
 
-        return memo[i][j];
+        return prev[n];
     }
 }
