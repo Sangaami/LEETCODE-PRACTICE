120fe3d2d6f6b5b878b119c45b5b441a13da11a4
Sangaami <msangaami@gmail.com>
2025-08-11 22:32:56 +0530

    Time: 2 ms (99.33%), Space: 43.5 MB (72.61%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -2,32 +2,16 @@ import java.util.*;
 
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        int len1 = nums1.length;
-        int len2 = nums2.length;
+        int[] ans = new int[nums1.length];
+        Map<Integer,Integer> idx = new HashMap<>();
+        for (int i = 0; i < nums2.length; i++) idx.put(nums2[i], i);
 
-        int[] ans = new int[len1];
-        HashMap<Integer, Integer> indexMap = new HashMap<>();
-
-        // Store index of each element in nums2
-        for (int i = 0; i < len2; i++) {
-            indexMap.put(nums2[i], i);
-        }
-
-        // For each element in nums1, search in nums2 from next index
-        for (int i = 0; i < len1; i++) {
-            int target = nums1[i];
-            int startIndex = indexMap.get(target) + 1;
-            int nextGreater = -1;
-
-            for (int j = startIndex; j < len2; j++) {
-                if (nums2[j] > target) {
-                    nextGreater = nums2[j];
-                    break;
-                }
+        for (int i = 0; i < nums1.length; i++) {
+            ans[i] = -1;
+            for (int j = idx.get(nums1[i]) + 1; j < nums2.length; j++) {
+                if (nums2[j] > nums1[i]) { ans[i] = nums2[j]; break; }
             }
-            ans[i] = nextGreater;
         }
-
         return ans;
     }
 }
