e141ef048c60c12171c546c029e4be1a9b8d8583
Sangaami <msangaami@gmail.com>
2025-09-01 21:12:29 +0530

    Time: 4 ms (57.45%), Space: 45.2 MB (46.81%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.java
+++ 0383-ransom-note/0383-ransom-note.java
@@ -1,15 +1,16 @@
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
-        int[] f = new int[256];
-        for (int i = 0; i < magazine.length(); i++) {
+        int [] f = new int[256];
+        for(int i=0; i<magazine.length(); i++){
             f[magazine.charAt(i)]++;
         }
-        for (int i = 0; i < ransomNote.length(); i++) {
-            if (--f[ransomNote.charAt(i)] < 0) {
+        for(int i=0; i<ransomNote.length(); i++){
+            f[ransomNote.charAt(i)]--;
+            if(f[ransomNote.charAt(i)] < 0){
                 return false; 
             }
         }
 
-        return true;
+        return true; 
     }
 }
