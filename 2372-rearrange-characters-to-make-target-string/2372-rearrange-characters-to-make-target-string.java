d2f31f77d3630ee0769e126ce6720d770ddd4384
Sangaami <msangaami@gmail.com>
2025-09-05 14:59:05 +0530

    Time: 0 ms (100%), Space: 41.7 MB (51.25%) - LeetHub

--- 2372-rearrange-characters-to-make-target-string/2372-rearrange-characters-to-make-target-string.java
+++ 2372-rearrange-characters-to-make-target-string/2372-rearrange-characters-to-make-target-string.java
@@ -1,45 +1,16 @@
-import java.util.*;
-
 class Solution {
     public int rearrangeCharacters(String s, String target) {
-        Stack<Character> stack = new Stack<>();
-        for (char ch : s.toCharArray()) {
-            stack.push(ch);
-        }
-
-        int count = 0;
-        while (true) {
-            StringBuilder formed = new StringBuilder();
-            Stack<Character> temp = new Stack<>();
-
-            for (int i = 0; i < target.length(); i++) {
-                char t = target.charAt(i);
-                boolean found = false;
-
-                while (!stack.isEmpty()) {
-                    char top = stack.pop();
-                    if (top == t && !found) {
-                        formed.append(top);
-                        found = true;
-                        break;
-                    } else {
-                        temp.push(top); // keep aside
-                    }
-                }
-
-                // restore skipped chars back into stack
-                while (!temp.isEmpty()) {
-                    stack.push(temp.pop());
-                }
-
-                if (!found) {
-                    return count; // can't build further
-                }
-            }
-
-            if (formed.toString().equals(target)) {
-                count++;
+        int[] freqS = new int[26]; 
+        int[] freqT = new int[26]; 
+        for (char ch : s.toCharArray()) freqS[ch - 'a']++;
+        for (char ch : target.toCharArray()) freqT[ch - 'a']++;
+        int result = Integer.MAX_VALUE;
+        for (int i = 0; i < 26; i++) {
+            if (freqT[i] > 0) {
+                if (freqS[i] == 0) return 0;
+                result = Math.min(result, freqS[i] / freqT[i]);
             }
         }
+        return result;
     }
 }
