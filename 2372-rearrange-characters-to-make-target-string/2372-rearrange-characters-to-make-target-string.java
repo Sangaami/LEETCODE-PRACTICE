951a10d5619008892852ae985db1c1a45bad4fa4
Sangaami <msangaami@gmail.com>
2025-09-05 14:51:07 +0530

    Time: 4 ms (3.62%), Space: 41.9 MB (25.07%) - LeetHub

--- 2372-rearrange-characters-to-make-target-string/2372-rearrange-characters-to-make-target-string.java
+++ 2372-rearrange-characters-to-make-target-string/2372-rearrange-characters-to-make-target-string.java
@@ -1,18 +1,45 @@
+import java.util.*;
+
 class Solution {
     public int rearrangeCharacters(String s, String target) {
-        int[] freqS = new int[26];
-        int[] freqT = new int[26];
+        Stack<Character> stack = new Stack<>();
         for (char ch : s.toCharArray()) {
-            freqS[ch - 'a']++;
+            stack.push(ch);
         }
-        for (char ch : target.toCharArray()) {
-            freqT[ch - 'a']++;
+
+        int count = 0;
+        while (true) {
+            StringBuilder formed = new StringBuilder();
+            Stack<Character> temp = new Stack<>();
+
+            for (int i = 0; i < target.length(); i++) {
+                char t = target.charAt(i);
+                boolean found = false;
+
+                while (!stack.isEmpty()) {
+                    char top = stack.pop();
+                    if (top == t && !found) {
+                        formed.append(top);
+                        found = true;
+                        break;
+                    } else {
+                        temp.push(top); // keep aside
+                    }
+                }
+
+                // restore skipped chars back into stack
+                while (!temp.isEmpty()) {
+                    stack.push(temp.pop());
+                }
+
+                if (!found) {
+                    return count; // can't build further
+                }
+            }
+
+            if (formed.toString().equals(target)) {
+                count++;
+            }
         }
-        int result = Integer.MAX_VALUE;
-        for (char ch : target.toCharArray()) {
-            int idx = ch - 'a';
-            result = Math.min(result, freqS[idx] / freqT[idx]);
-        }
-        return result;
     }
 }
